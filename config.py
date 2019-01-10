f9789de45839eed4aa1f4a864db3af4585673f96
Kouthar W <32654489+kwaled@users.noreply.github.com>
2019-01-10 15:47:52 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 #your account's username
-username = "StarterHacksBot"
+username = ""
 
 #your account's password
-password = "Start3rH4acks!!"
+password = ""
 
 #upper left corner beneath "personal use script"
-client_id = "F6vcttHEE7FOEQ"
+client_id = ""
 
 #the key that's given next to "secret"
-client_secret =  "HH1a3v5Lvumg0sTgWFCj33rIVJ4"
+client_secret =  ""
 
 #includes brief, true description of bot with version number
 user_agent = "General bot created for workshop v.1"
